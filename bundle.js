7b3189d611131492fb6dbda35519cd1eec232168
Marco Barão <marco.barao@outlook.com>
2018-10-20 18:42:03 -0300

    Arrow functions

--- bundle.js
+++ bundle.js
@@ -1,19 +1,17 @@
 "use strict";
 
-var arr = [1, 3, 4, 6, 7, 9, 11, 15, 16];
-var map = arr.map(function (item) {
+var arr = [1, 2, 4, 5, 6];
+var newArr = arr.map(function (item) {
   return item * 2;
 });
-console.log(map);
-var reduce = arr.reduce(function (total, item) {
-  return total += item;
-});
-console.log(reduce);
-var filter = arr.filter(function (item) {
-  return item % 2 == 0;
-});
-console.log(filter);
-var find = arr.find(function (item) {
-  return item === 4;
-});
-console.log(find);
+console.log(newArr); // const test = () => {
+//   return { nome: "Marco" };
+// };
+
+var test = function test() {
+  return {
+    nome: "Marco"
+  };
+};
+
+console.log(test());
--- main.js
+++ main.js
@@ -1,25 +1,13 @@
-const arr = [1, 3, 4, 6, 7, 9, 11, 15, 16];
+const arr = [1, 2, 4, 5, 6];
 
-const map = arr.map(function(item) {
-  return item * 2;
-});
+const newArr = arr.map(item => item * 2);
 
-console.log(map);
+console.log(newArr);
 
-const reduce = arr.reduce(function(total, item) {
-  return (total += item);
-});
+// const test = () => {
+//   return { nome: "Marco" };
+// };
 
-console.log(reduce);
+const test = () => ({ nome: "Marco" });
 
-const filter = arr.filter(function(item) {
-  return item % 2 == 0;
-});
-
-console.log(filter);
-
-const find = arr.find(function(item) {
-  return item === 4;
-});
-
-console.log(find);
+console.log(test());
