8f1ec3049069ad57a0d18bc895a538755b73519d
Marco Barão <marco.barao@outlook.com>
2018-10-20 18:15:16 -0300

    Classes

--- main.js
+++ main.js
@@ -1,4 +1,26 @@
-class Teste {
-  teste() {}
-  teste2() {}
+class List {
+  constructor() {
+    this.list = [];
+  }
+
+  add(elem) {
+    this.list.push(elem);
+    return this;
+  }
+
+  get() {
+    return this.list;
+  }
 }
+
+class TodoList extends List {
+  constructor() {
+    super();
+  }
+  static oQueEuSou() {
+    return "Eu sou uma lista de afazeres";
+  }
+}
+
+console.log(new TodoList().add("algumacoisa").get());
+console.log(TodoList.oQueEuSou());
