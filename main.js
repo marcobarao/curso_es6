8d720d27bc2ace23d2d22349ad68a82382e73bdd
Marco Barão <marco.barao@outlook.com>
2018-10-20 19:13:52 -0300

    Operadores rest/spread

--- main.js
+++ main.js
@@ -1,14 +1,41 @@
+// REST
+
 const usuario = {
-  nome: "Marco",
-  idade: 18,
-  endereco: {
-    cidade: "São Paulo",
-    estado: "SP"
-  }
+  nome: "Diego",
+  idade: 23,
+  empresa: "Rocketseat"
 };
 
-function perfilUsuario({ nome, idade, endereco: { estado } }) {
-  console.log(`O usuário ${nome} tem ${idade} e mora em ${estado}`);
-}
+const { nome, ...resto } = usuario;
+console.log(nome);
+console.log(resto);
 
-perfilUsuario(usuario);
+const arr = [1, 2, 3, 4];
+const [a, b, ...c] = arr;
+console.log(a);
+console.log(b);
+console.log(c);
+
+const sum = (...params) => {
+  return params.reduce((total, item) => {
+    return (total += item);
+  });
+};
+console.log(sum);
+
+// SPREAD
+
+const arr1 = [1, 2, 3, 4];
+const arr2 = [5, 6, 7, 8];
+
+const arr3 = [...arr1, ...arr2];
+console.log(arr3);
+
+const usuario1 = {
+  nome: "Marco",
+  idade: 18,
+  empresa: "Nenhuma"
+};
+
+const usuario2 = { ...usuario1, nome: "Gabriel" };
+console.log(usuario2);
