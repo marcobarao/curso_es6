01e57b911a595e5f942c3253bb2ddb1bc6eed43c
Marco Barão <marco.barao@outlook.com>
2018-10-21 09:07:59 -0300

    Import e export

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-const nome = "Marco";
-const idade = 18;
+import soma, { sub, mult, div } from "./funcoes";
+// import * as functions from "./funcoes";
 
-const usuario = {
-  nome,
-  idade,
-  empresa: "Nenhuma"
-};
+console.log(soma(1, 2));
+console.log(sub(6, 2));
+console.log(mult(2, 1));
+console.log(div(9, 3));
 
-console.log(usuario);
+// console.log(functions.soma(1, 2));
+// console.log(functions.sub(1, 2));
